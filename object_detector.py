c2f8ec45ad52f17b4e353c5feb129ea96c9ce31d
Evgen Kostenko <ekostenko@spscommerce.com>
2017-11-01 12:24:06 +0200

    Update inteface

--- object_detector.py
+++ object_detector.py
@@ -1,8 +1,13 @@
-from abc import ABCMeta
+from abc import ABC, abstractmethod
 
 
-class ObjectDetector(metaclass=ABCMeta):
+class ObjectDetector(ABC):
 
     @abstractmethod
-    def calculate_distance_to_object_in_front():
+    def calculate_distance_to_object_in_front(x):
+        """
+        Calculate distance to object
+        :param x: int deinstalation
+        :return int distance to object
+        """
         pass
